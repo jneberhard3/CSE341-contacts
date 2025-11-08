10a8bb7b716db9e0c02d1f85c5d42b34ce727f9a
jneberhard3 <jimtechweb@gmail.com>
2025-11-07 22:38:59 -0700

    week2 swagger upload with CRUD

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,8 @@ const contactsController = require("../controllers/contacts");
 router.get("/", contactsController.getAll);
 router.get("/:id", contactsController.getSingle);
 
+router.post("/", contactsController.createContact);  //create contact
+router.put("/:id", contactsController.updateContact);  //update contact
+router.delete("/:id", contactsController.deleteContact);   //delete contact
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Z-key");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 app.use("/", require("./routes"));
 
 
@@ -17,4 +25,6 @@ mongodb.initDb((err) => {
         })
     }
 })
+
+
 
